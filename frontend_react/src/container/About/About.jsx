9370dedb6929fa9ab0ebce3e3263cce7fd08d730
Zarrar Jawed Ahmad Palekar <zarrar.palekar@syndicationpro.com>
2022-02-21 21:48:09 +0530

    Initial integration with sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -4,25 +4,22 @@ import { motion } from "framer-motion";
 import { images } from "../../constants";
 
 import "./About.scss";
+import { client, urlFor } from "../../client";
 
-const abouts = [
-  {
-    title: "Full Stack Developer",
-    description: "I am a good full stack developer.",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Web Developer",
-    description: "I am a good web developer.",
-    imgUrl: images.about02,
-  },
-];
+import { AppWrap } from "../../wrapper";
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
-        I Know That <span>Good Design</span> <br /> means{" "}
+        I Know That <span>Good Web Apps</span> <br /> means{" "}
         <span>Good Business</span>
       </h2>
       <div className="app__profiles">
@@ -34,7 +31,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
@@ -48,4 +45,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, "about");
